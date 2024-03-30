29c11bb99b338edfd4229bb5fb852d1165cfb0d9
Alper <wildborn06@yahoo.com>
2024-03-30 14:02:49 +0300

    Initial Commit

---
+++ TatliKrizim/TatliKrizimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TatliKrizimApp.swift
+//  TatliKrizim
+//
+//  Created by Alper Erkoca on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TatliKrizimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
